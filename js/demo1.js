ffb16a84918d06ec2e3967588bba329de5f32735
Tim Indermuehle <ind7734@gmail.com>
2021-05-29 19:34:46 -0600

    Add files via upload
    initial commit

---
+++ js/demo1.js
@@ -0,0 +1,112 @@
+/**
+ * demo1.js
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2018, Codrops
+ * http://www.codrops.com
+ */
+{
+    const chars = ['$', '%', '#', '@', '&', '(', ')', '=', '*', '/'];
+    const charsTotal = chars.length;
+    const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+    class Entry {
+        constructor(el) {
+            this.DOM = {
+                el: el
+            };
+            this.DOM.image = this.DOM.el.querySelector('.content__img');
+            this.DOM.title = {
+                word: this.DOM.el.querySelector('.content__text')
+            };
+            charming(this.DOM.title.word);
+            this.DOM.title.letters = Array.from(this.DOM.title.word.querySelectorAll('span'));
+            this.DOM.title.letters.forEach(letter => letter.dataset.initial = letter.innerHTML);
+            this.lettersTotal = this.DOM.title.letters.length;
+            observer.observe(this.DOM.el);
+        }
+        enter(direction = 'down') {
+            this.DOM.title.word.style.opacity = 1;
+
+            this.timeouts = [];
+            this.complete = false;
+            let cnt = 0;
+            this.DOM.title.letters.forEach((letter, pos) => {
+                const timeout = setTimeout(() => {
+                    letter.innerHTML = chars[getRandomInt(0, charsTotal - 1)];
+                    setTimeout(() => {
+                        letter.innerHTML = letter.dataset.initial;
+                        ++cnt;
+                        if (cnt === this.lettersTotal) {
+                            this.complete = true;
+                        }
+                    }, 100);
+                }, pos * 80);
+                this.timeouts.push(timeout);
+            });
+        }
+        exit(direction = 'down') {
+            this.DOM.title.word.style.opacity = 0;
+            if (this.complete) return;
+            for (let i = 0, len = this.timeouts.length; i <= len - 1; ++i) {
+                clearTimeout(this.timeouts[i]);
+            }
+        }
+    }
+
+    let observer;
+    let current = -1;
+    let allentries = [];
+    const sections = Array.from(document.querySelectorAll('.content__section'));
+
+    // Preload all the images in the page..
+    imagesLoaded(document.querySelectorAll('.content__img'), () => {
+        document.body.classList.remove('loading');
+        if ('IntersectionObserver' in window) {
+            document.body.classList.add('ioapi');
+
+            observer = new IntersectionObserver((entries) => {
+                entries.forEach(entry => {
+                    if (entry.intersectionRatio > 0.5) {
+                        const newcurrent = sections.indexOf(entry.target);
+                        if (newcurrent === current) return;
+                        const direction = newcurrent > current;
+                        if (current >= 0) {
+                            allentries[current].exit(direction ? 'down' : 'up');
+                        }
+                        allentries[newcurrent].enter(direction ? 'down' : 'up');
+                        current = newcurrent;
+                    }
+                });
+            }, {
+                threshold: 0.5
+            });
+
+            sections.forEach(section => allentries.push(new Entry(section)));
+        }
+    });
+}
+
+// js for grand staircase modal
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
